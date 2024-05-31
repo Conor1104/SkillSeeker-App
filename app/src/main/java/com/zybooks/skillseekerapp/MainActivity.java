4ac6adeac52e5bd79c0e9f2a3b4f7a929e9a4d3c
Pogobro <119134397+Pogobro@users.noreply.github.com>
2024-05-30 20:52:26 -0400

    Update MainActivity.java
    For SSDataBaseHelper

--- app/src/main/java/com/zybooks/skillseekerapp/MainActivity.java
+++ app/src/main/java/com/zybooks/skillseekerapp/MainActivity.java
@@ -10,5 +10,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //SSDataBaseHelper stuff
+        //object of database class is created
+        SSDataBaseHelper SSDBhelper= new SSDataBaseHelper(this);
+        //test dummy
+        //calls function and data is passed to addUser In SSDataBaseHelper
+        SSDBhelper.addUser("Will", "23525637");
     }
 }
