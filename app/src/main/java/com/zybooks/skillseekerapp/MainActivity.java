f8f3c1c607c80614ed5a84c55fa6f23bdd46b54a
Pogobro <119134397+Pogobro@users.noreply.github.com>
2024-06-01 18:50:33 -0400

    Update MainActivity.java

--- app/src/main/java/com/zybooks/skillseekerapp/MainActivity.java
+++ app/src/main/java/com/zybooks/skillseekerapp/MainActivity.java
@@ -21,12 +21,20 @@ public class MainActivity extends AppCompatActivity {
 
         //SSDataBaseHelper stuff
         //object of database class is created
-       SSDataBaseHelper SS_USER_DATA_BASE= new SSDataBaseHelper(this);
+        SSDataBaseHelper SS_USER_DATA_BASE= new SSDataBaseHelper(this);
         //test dummy
         //calls function and data is passed to addUser In SSDataBaseHelper
-        SS_USER_DATA_BASE.addUser("Will", "23525637" , "23");
 
-        SS_USER_DATA_BASE.addFreelancer("Mr. Freelancer", "678493349" , "5");
+        //Example for adding users and Freelancers
+        //SS_USER_DATA_BASE.addUser("Will", "23525637" , "23");
+        //SS_USER_DATA_BASE.addFreelancer("Mr. Freelancer", "678493349" , "5");
+
+        ArrayList<ModalUser> data= SS_USER_DATA_BASE.fetchUser();
+
+        //Test displays print of data from data set in log
+        for(int i = 0; i < data.size(); i++){
+            Log.d("User info ", "Name " + data.get(i).name + " Phone Num " + data.get(i).phone_num + " Age " + data.get(i).age);
+        }
 
 
 
