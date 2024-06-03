09635583a122316e5fda057fca2b849a4e71c16f
BitOfAction <99789176+BitOfAction@users.noreply.github.com>
2024-06-02 21:45:36 -0400

    Freelancer table added

--- app/src/main/java/com/zybooks/skillseekerapp/Login_Register.java
+++ app/src/main/java/com/zybooks/skillseekerapp/Login_Register.java
@@ -1,6 +1,8 @@
 package com.zybooks.skillseekerapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,4 +23,10 @@ public class Login_Register extends AppCompatActivity {
             return insets;
         });
     }
+
+    public void goto_activity_main (View view){
+        Intent intent = new Intent (this, MainActivity.class);
+        startActivity(intent);
+    }
+
 }
