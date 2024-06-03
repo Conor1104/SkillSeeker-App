dd66aec922b487376d3b326905df44546c0c906b
BitOfAction <99789176+BitOfAction@users.noreply.github.com>
2024-06-03 02:43:22 -0400

    Freelancer table added

--- app/src/main/java/com/zybooks/skillseekerapp/register_page.java
+++ app/src/main/java/com/zybooks/skillseekerapp/register_page.java
@@ -1,6 +1,8 @@
 package com.zybooks.skillseekerapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,11 +16,16 @@ public class register_page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_register_page);
+        setContentView(R.layout.activity_login_page);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
+
+    public void goto_login_page (View view){
+        Intent intent = new Intent (this, Login_Register.class);
+        startActivity(intent);
+    } // Goes to main login page (Login's Back Button)
 }
