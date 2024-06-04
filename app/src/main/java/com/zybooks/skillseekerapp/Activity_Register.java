63323c333ae3468ad594f4881d14586d99832543
Pogobro <119134397+Pogobro@users.noreply.github.com>
2024-06-03 22:02:23 -0400

    Logging in
    -Updates–
    -Creating your user or freelancer account will take you to login page once your account is created
    -Expanded freelancer and User database
    -User’s and Freelancer can login BUT there is a bug that needs to be fixed. A user can sign in with a Freelancers password and vice versa. I think it also can be done with usernames so both need to be fixed

--- app/src/main/java/com/zybooks/skillseekerapp/Activity_Register.java
+++ app/src/main/java/com/zybooks/skillseekerapp/Activity_Register.java
@@ -18,11 +18,16 @@ public class Activity_Register extends AppCompatActivity {
     private EditText nameInput;
     private EditText phoneInput;
     private EditText ageInput;
+
+    private EditText userPassword;
+
+    private EditText userEmail;
     private Button UserSubmitButton;
     private EditText freelancerNameInput;
     private EditText freelancerExpInput;
     private EditText freelancerReviewInput;
     private Button freelancerSubmitButton;
+    private EditText freelancerPasswordInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,12 +42,15 @@ public class Activity_Register extends AppCompatActivity {
         nameInput = findViewById(R.id.nameInput);
         phoneInput = findViewById(R.id.phoneInput);
         ageInput = findViewById(R.id.ageInput);
+        userPassword = findViewById(R.id.userPassword);
+        userEmail = findViewById(R.id.userEmail);
         UserSubmitButton = findViewById(R.id.UserSubmitButton);
 
         freelancerNameInput = findViewById(R.id.freelancerNameInput);
         freelancerExpInput = findViewById(R.id.freelancerExpInput);
         freelancerReviewInput = findViewById(R.id.freelancerReviewInput);
         freelancerSubmitButton = findViewById(R.id.freelancerSubmitButton);
+        freelancerPasswordInput = findViewById(R.id.freelancerPasswordInput);
 
 
 
@@ -53,12 +61,16 @@ public class Activity_Register extends AppCompatActivity {
                 String name = nameInput.getText().toString();
                 String phone = phoneInput.getText().toString();
                 String age = ageInput.getText().toString();
+                String user_password = userPassword.getText().toString();
+                String user_email = userEmail.getText().toString();
 
                 //Error handling
-                if (!name.isEmpty() && !phone.isEmpty() && !age.isEmpty()) {
-                    dbHelper.addUser(name, phone, age);
+                if (!name.isEmpty() && !phone.isEmpty() && !age.isEmpty() &&!user_password.isEmpty() && !user_email.isEmpty()) {
+                    dbHelper.addUser(name, phone, age, user_password, user_email);
                     Toast.makeText(Activity_Register.this, "User added", Toast.LENGTH_SHORT).show();
                     clearInputs();
+                    //Will take the User to the login page to login to there new account
+                    goto_login_page();
                 } else {
                     Toast.makeText(Activity_Register.this, "Please fill all fields", Toast.LENGTH_SHORT).show();
                 }
@@ -71,12 +83,16 @@ public class Activity_Register extends AppCompatActivity {
                 String freelancer_name = freelancerNameInput.getText().toString();
                 String experience = freelancerExpInput.getText().toString();
                 String review_stars = freelancerReviewInput.getText().toString();
+                String freelancer_password = freelancerPasswordInput.getText().toString();
 
-                if (!freelancer_name.isEmpty() && !experience.isEmpty() && !review_stars.isEmpty()) {
-                    dbHelper.addFreelancer(freelancer_name, experience, review_stars );
+                if (!freelancer_name.isEmpty() && !experience.isEmpty() && !review_stars.isEmpty() && !freelancer_password.isEmpty()) {
+                    dbHelper.addFreelancer(freelancer_name, experience, review_stars,freelancer_password );
                     Toast.makeText(Activity_Register.this, "User added", Toast.LENGTH_SHORT).show();
                     clearInputs();
-                } else {
+                    //Will take the Freelancer to the login page to login to there new account
+                    goto_login_page();
+                }
+                else {
                     Toast.makeText(Activity_Register.this, "Please fill all fields", Toast.LENGTH_SHORT).show();
                 }
             }
@@ -85,13 +101,13 @@ public class Activity_Register extends AppCompatActivity {
         //Test for fetching and logging users
         ArrayList<ModalUser> userData = dbHelper.fetchUser();
         for (ModalUser user : userData) {
-            Log.d("User info", "ID: " + user.user_id + " Name: " + user.name + " Phone Num: " + user.phone_num + " Age: " + user.age);
+            Log.d("User info", "ID: " + user.user_id + " Name: " + user.name + " Phone Num: " + user.phone_num + " Age: " + user.age + " Password: " + user.user_password + " Email: " + user.user_email);
         }
 
         // Test: fetch and log freelancers
         ArrayList<ModalFreelancer> freelancerData = dbHelper.fetchFreelancer();
         for (ModalFreelancer freelancer : freelancerData) {
-            Log.d("Freelancer info", "ID: " + freelancer.freelancer_id + " Name: " + freelancer.name + " Experience: " + freelancer.experience + " Star Review: " + freelancer.starReview);
+            Log.d("Freelancer info", "ID: " + freelancer.freelancer_id + " Name: " + freelancer.name + " Experience: " + freelancer.experience + " Star Review: " + freelancer.starReview + " Freelancer Password: " + freelancer.freelancerPassword);
         }
 
         //Changes data in the data set. Changing phone num example
@@ -107,12 +123,15 @@ public class Activity_Register extends AppCompatActivity {
         nameInput.setText("");
         phoneInput.setText("");
         ageInput.setText("");
+        userPassword.setText("");
+        userEmail.setText("");
     }
 
     private void clearFreelancerInputs() {
         freelancerNameInput.setText("");
         freelancerExpInput.setText("");
         freelancerReviewInput.setText("");
+        freelancerPasswordInput.setText("");
     }
     //test dummy
     //calls function and data is passed to addUser In SSDataBaseHelper
@@ -127,6 +146,13 @@ public class Activity_Register extends AppCompatActivity {
             Log.d("User info ", "Name " + data.get(i).name + " Phone Num " + data.get(i).phone_num + " Age " + data.get(i).age);
         }*/
 
+    //Not Button
+    public void goto_login_page(){
+        Intent intent = new Intent (this, Login_Register.class);
+        startActivity(intent);
+    }
+
+    //Button
     public void goto_main(View view){
         Intent intent = new Intent (this, MainActivity.class);
         startActivity(intent);
